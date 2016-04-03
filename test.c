b2c0b6f3884e6ffa6532634ca5420c1782267db5
Matt Mendell <matthew.mendell@gmail.com>
2016-04-02 20:11:40 -0400

    Add a large test case

--- test.c
+++ test.c
@@ -36,8 +36,28 @@ void test_two_heap_entry() {
     destroy_heap();
 }
 
+void test_many_heap_entry() {
+    printf("Testing many heap entry... ");
+
+    create_heap();
+    for (int i = 100; i > 0; i -= 1) {
+        add_to_heap(i);
+    }
+    print_heap();
+    int max = peek_heap();
+    if (max != 100) {
+        printf("ERROR: Expected 100, but got %d\n", max);
+    }
+    else {
+        printf("Pass\n");
+    }
+
+    destroy_heap();
+}
+
 int main (int argc, char * argv[]) {
     test_one_heap_entry();
     test_two_heap_entry();
+    test_many_heap_entry();
 }
 
